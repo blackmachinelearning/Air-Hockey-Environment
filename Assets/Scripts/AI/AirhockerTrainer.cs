1986de7acf17052734a2bd64b8a82d7df59ce7f0
drogovozDP <choper.2013@bk.ru>
2024-03-17 19:04:38 +0400

    update training script

--- Assets/Scripts/AI/AirhockerTrainer.cs
+++ Assets/Scripts/AI/AirhockerTrainer.cs
@@ -5,43 +5,12 @@ using Random = UnityEngine.Random;
 
 public class AirhockerTrainer : MonoBehaviour
 {
-    private void Start()
-    {
-
-    }
-    
-    private void FixedUpdate()
-    {
- 
-    }
-    
-    public void HitWasher(string playerTag)
-    {
-
-    }
+    public void HitWasher(string playerTag) { }
 
     public void Goal(string goalGateTag)
     {
         ResetGame(goalGateTag);
     }
 
-    public void ResetGame(string goalGateTag)
-    {
-        
-    }
-
-    private void ResetBlue()
-    {
-        
-    }
-
-    private void ResetRed()
-    {
-        
-    }
-
-    private void ResetBoth(string goalGateTag)
-    {
-
-    }
+    public void ResetGame(string goalGateTag) { }
 }
